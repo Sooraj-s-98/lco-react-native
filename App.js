1ce8ea4c7e7d4e2f2b9c27008aeb82b0e5b5d795
sooraj <sooraj1998ayiroor@gmail.com>
2020-12-19 17:21:34 +0530

    assignment 2 done

--- App.js
+++ App.js
@@ -15,9 +15,14 @@ import DiceFive from './assets/dice5.png';
 import DiceSix from './assets/dice6.png';
 const App = () =>{
   const [uri,setUri]=useState(DiceOne);
+  const [uritwo,setUritwo]=useState(DiceOne);
   const PlayButtonTapped=()=>{
-    let randomnumber=Math.floor(Math.random()*6)+1;
-    switch(randomnumber){
+    FirstDice();
+    SecondDice();
+  };
+  const FirstDice=()=>{
+    let randomnumberone=Math.floor(Math.random()*6)+1;
+    switch(randomnumberone){
       case 1:
              setUri(DiceOne);
              break;
@@ -39,12 +44,39 @@ const App = () =>{
       default: setUri(DiceOne)                              
     }
   }
-
+  const SecondDice=()=>{
+    let randomnumbertwo=Math.floor(Math.random()*6)+1;
+    switch(randomnumbertwo){
+      case 1:
+             setUritwo(DiceOne);
+             break;
+      case 2:
+             setUritwo(DiceTwo);
+             break;
+      case 3:
+             setUritwo(DiceThree);
+             break;
+      case 4:
+             setUritwo(DiceFour);
+             break;
+      case 5:
+              setUritwo(DiceFive);
+              break;
+      case 6:
+              setUritwo(DiceSix);
+              break;
+      default: setUri(DiceOne)                              
+    }
+  }
   return(
     <>
       <View style={styles.container}>
-        <Pressable onPress={PlayButtonTapped}>
+        <View style={styles.dice}>
            <Image style={styles.image} source={uri} />
+           <Image style={styles.image} source={uritwo} />
+        </View>
+        <Pressable onPress={PlayButtonTapped}>
+          <Text style={styles.gamePlayButton}>play game</Text>
         </Pressable>
       </View>
     </>
@@ -59,8 +91,22 @@ const styles=StyleSheet.create({
     justifyContent : 'center',
     backgroundColor: "#222831"
   },
+  gamePlayButton :{
+     fontSize:20,
+     marginTop:30,
+     color:"#F2A365",
+     paddingHorizontal: 40,
+     paddingVertical : 10,
+     borderColor : "#30475E",
+     borderRadius:5,
+     borderWidth: 3,
+     fontWeight: "bold"
+  },
   image :{
      width:200,
      height:200,
+  },
+  dice:{
+    flexDirection: 'row',
   }
 })
