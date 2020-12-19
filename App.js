cd8715d0f9f6febcddf2932c9019fbc0a42aafdd
sooraj <sooraj1998ayiroor@gmail.com>
2020-12-19 14:18:50 +0530

    function done

--- App.js
+++ App.js
@@ -14,13 +14,37 @@ import DiceFour from './assets/dice4.png';
 import DiceFive from './assets/dice5.png';
 import DiceSix from './assets/dice6.png';
 const App = () =>{
-  const [uri,setUri]=useState(DiceOne)
+  const [uri,setUri]=useState(DiceOne);
+  const PlayButtonTapped=()=>{
+    let randomnumber=Math.floor(Math.random()*6)+1;
+    switch(randomnumber){
+      case 1:
+             setUri(DiceOne);
+             break;
+      case 2:
+             setUri(DiceTwo);
+             break;
+      case 3:
+             setUri(DiceThree);
+             break;
+      case 4:
+             setUri(DiceFour);
+             break;
+      case 5:
+              setUri(DiceFive);
+              break;
+      case 6:
+              setUri(DiceSix);
+              break;
+      default: setUri(DiceOne)                              
+    }
+  }
 
   return(
     <>
       <View style={styles.container}>
         <Image style={styles.image} source={uri} />
-        <TouchableOpacity>
+        <TouchableOpacity onPress={PlayButtonTapped}>
           <Text style={styles.gamePlayButton}>play game</Text>
         </TouchableOpacity>
       </View>
