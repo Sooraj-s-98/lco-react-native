2e989023bd52028b60cd0a96dc7582cac91f6780
sooraj <sooraj1998ayiroor@gmail.com>
2020-12-19 11:15:18 +0530

    stylling 1

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import React from "react";
 import {
   Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
 } from "react-native";
 
 const App = () =>{
 
   return(
     <>
+    <View style={[styles.container,{ backgroundColor :"rgb(32,0,126)"}]}>
     <Text>SOORAJ S</Text>
+    </View>
     </>
   );
 };
 export default App;
+
+const styles=StyleSheet.create({
+  container :{
+    flex : 1,
+    alignItems: 'center',
+    justifyContent : 'center',
+  },
+})
