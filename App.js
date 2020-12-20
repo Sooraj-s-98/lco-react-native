59a9310c3fe3aa72d4779deabbeab2b9f5920a5d
sooraj <sooraj1998ayiroor@gmail.com>
2020-12-20 19:55:53 +0530

    currency app done

--- App.js
+++ App.js
@@ -4,36 +4,73 @@ import {
   StyleSheet,
   View,
   TouchableOpacity,
+  ScrollView,
+  SafeAreaView,
+  TextInput,
+
 } from "react-native";
 
+import Snackbar from 'react-native-snackbar';
+
+const currencyPerRupee={
+  DOLLAR:0.014,
+  EURO:0.012,
+  POUND:0.011,
+  RUBEL:0.93,
+  AUSDOLLAR:0.2,
+  CANDOLLAR:0.019,
+  YEN:1.54,
+  DINAR:0.0043,
+  BITCOIN:0.000004,
+}
+
 const App = () =>{
+const [inputValue,setInputValue]=useState(0);
+const [resultValue,setResultValue]=useState(0);
 
-  const [randomColor,setRandomColor]=useState("rgb(32,0,126)");
-  
-  const resetBG=()=>{
-    setRandomColor("rgb(0,0,0)");
+const buttonPressed=(currency)=>{
+  if(!inputValue){
+    return Snackbar.show({
+      text: 'Please enter value',
+      backgroundColor:"#EA7773",
+      textColor:"#FFFFFF",
+    });
   };
-  const changeBG=()=>{
-
-    let color="rgb("+
-      Math.floor(Math.random()*256) +
-      ','+
-      Math.floor(Math.random()*256) +
-      ','+
-      Math.floor(Math.random()*256) +
-      ')';
-      setRandomColor(color);
-   };
+  let result=parseFloat(inputValue) * currencyPerRupee[currency];
+  setResultValue(result.toFixed(2));
+}
   return(
     <>
-    <View style={[styles.container,{ backgroundColor : randomColor}]}>
-      <TouchableOpacity onPress={changeBG}>
-        <Text style={styles.text}>TAP ME</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={resetBG}>
-        <Text style={styles.reset}>Reset</Text>
-      </TouchableOpacity>
-    </View>
+     <ScrollView keyboardShouldPersistTaps="handled"
+     contentInsetAdjustmentBehavior="automatic">
+       <SafeAreaView style={styles.container}>
+          <View style={styles.resultContainer}>
+            <Text style={styles.resultValue}>{resultValue}</Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput 
+            style={styles.input}
+            keyboardType="numeric"
+            placeholder="Enter Value"
+            placeholderTextColor="#c1c1c1" 
+            value={inputValue}
+            onChangeText={(inputValue)=> setInputValue(inputValue)}>
+            </TextInput>
+          </View>
+          <View  style={styles.covertButtonContainer}>
+             {
+               Object.keys(currencyPerRupee).map((currency)=>(
+                    <TouchableOpacity
+                    key={currency}
+                    style={styles.converterButton}
+                    onPress={()=>buttonPressed(currency)}>
+                      <Text style={styles.covertButtonText}>{currency}</Text>
+                    </TouchableOpacity>
+               ))
+             }
+          </View>
+       </SafeAreaView>
+     </ScrollView>
     </>
   );
 };
@@ -42,24 +79,50 @@ export default App;
 const styles=StyleSheet.create({
   container :{
     flex : 1,
-    alignItems: 'center',
-    justifyContent : 'space-evenly',
+    backgroundColor:"#1b262c"
   },
-  text :{
-    fontSize :30,
-    backgroundColor: "#BB2CD9",
-    paddingHorizontal:40,
-    color: "#FFFFFF",
-    borderRadius : 15,
-    textTransform : "uppercase"
+  resultContainer:{
+    height:70,
+    marginTop:80,
+    justifyContent:"center",
+    borderColor:"#bbe1fa",
+    borderWidth:2,
+    alignItems:"center",
   },
-  reset :{
-    fontSize :20,
-    backgroundColor: "#FFFFFF",
-    paddingVertical:2,
-    paddingHorizontal:10,
-    color: "#000000",
-    borderRadius : 10,
-    textTransform : "uppercase"
+  resultValue:{
+    fontSize:30,
+    color:"#FFF",
+    fontWeight:"bold",
+  },
+  inputContainer:{
+      height:70,
+      marginTop:10,
+      justifyContent:"center",
+      borderWidth:2,
+      borderColor:"#bbe1fa",
+  },
+  input:{
+    fontSize:30,
+    textAlign:"center",
+    color:"#FFFFFF",
+  },
+  covertButtonContainer:{
+    flexDirection:"row",
+    flexWrap:"wrap",
+    marginTop:10,
+  },
+  converterButton :{
+    alignItems:"center",
+    justifyContent:"center",
+    height:100,
+    width:"33.3%",
+    borderColor:"#bbe1fa",
+    borderWidth:2,
+    marginTop:10,
+    backgroundColor:"#0f4c75"
+  },
+  covertButtonText:{
+    color:"#FFF",
+    fontSize:15,
   }
-})
+});
