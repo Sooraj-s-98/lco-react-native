4b358f8e6e5c71fdab9ead62754869b280f335f4
sooraj <sooraj1998ayiroor@gmail.com>
2020-12-20 06:57:09 +0530

    done spanish number

--- App.js
+++ App.js
@@ -1,40 +1,57 @@
-import React, {useState} from "react";
+import React,{useState} from "react";
 import {
   Text,
   StyleSheet,
   View,
-  TouchableOpacity,
+  ScrollView,
+  Image,
+  Pressable,
 } from "react-native";
 
+import Sound from 'react-native-sound';
+
+const soundList=[
+  require('./assets/one.wav'),
+  require('./assets/two.wav'),
+  require('./assets/three.wav'),
+  require('./assets/four.wav'),
+  require('./assets/five.wav'),
+  require('./assets/six.wav'),
+  require('./assets/seven.wav'),
+  require('./assets/eight.wav'),
+  require('./assets/nine.wav'),
+  require('./assets/ten.wav'),
+];
+
 const App = () =>{
+  const playSound=(sound)=>{
+  const soundvar=new Sound(sound, Sound.MAIN_BUNDLE, (err)=>{
+    if(err){
+      console.log("NOT ABLE TO PLAY SOUND")
+    }
+  });
 
-  const [randomColor,setRandomColor]=useState("rgb(32,0,126)");
-  
-  const resetBG=()=>{
-    setRandomColor("rgb(0,0,0)");
-  };
-  const changeBG=()=>{
-
-    let color="rgb("+
-      Math.floor(Math.random()*256) +
-      ','+
-      Math.floor(Math.random()*256) +
-      ','+
-      Math.floor(Math.random()*256) +
-      ')';
-      setRandomColor(color);
-   };
+  setTimeout(()=>{
+    soundvar.play();
+  },1000);
+  soundvar.release();
+};
   return(
-    <>
-    <View style={[styles.container,{ backgroundColor : randomColor}]}>
-      <TouchableOpacity onPress={changeBG}>
-        <Text style={styles.text}>TAP ME</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={resetBG}>
-        <Text style={styles.reset}>Reset</Text>
-      </TouchableOpacity>
-    </View>
-    </>
+    <ScrollView style={styles.container}>
+      <Image source={require('./assets/logo.png')} style={styles.logo}/>
+      <View style={styles.gridContainer}>
+        {soundList.map((sound)=>(
+          <Pressable
+          key={sound}
+          style={styles.box}
+          onPress={()=>{playSound(sound)}}>
+            <Text style={styles.text}>
+              {sound}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
+    </ScrollView>   
   );
 };
 export default App;
@@ -42,24 +59,32 @@ export default App;
 const styles=StyleSheet.create({
   container :{
     flex : 1,
-    alignItems: 'center',
-    justifyContent : 'space-evenly',
+    backgroundColor: "#1b262c",
   },
-  text :{
-    fontSize :30,
-    backgroundColor: "#BB2CD9",
-    paddingHorizontal:40,
-    color: "#FFFFFF",
-    borderRadius : 15,
-    textTransform : "uppercase"
+  logo:{
+    alignSelf: "center",
+    marginTop :15,
   },
-  reset :{
-    fontSize :20,
-    backgroundColor: "#FFFFFF",
-    paddingVertical:2,
-    paddingHorizontal:10,
-    color: "#000000",
-    borderRadius : 10,
-    textTransform : "uppercase"
+  gridContainer:{
+   flex:1,
+   margin:5,
+   flexDirection:"row",
+   flexWrap: "wrap",
+   alignItems :"flex-start",
+   justifyContent:"space-around",
+  },
+  text:{
+    fontSize:50,
+    color:"#ff4301",
+  },
+  box:{
+    height:110,
+    alignItems:"center",
+    justifyContent :"center",
+    width:"46%",
+    marginVertical:6,
+    backgroundColor:"#0f4c75",
+    borderRadius:5,
+    elevation:5,
   }
 })
